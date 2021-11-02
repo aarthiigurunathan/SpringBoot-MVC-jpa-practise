71ef13604e017351830f3ee258a67fd0d85fd896
aarthiigurunathan <36591431+aarthiigurunathan@users.noreply.github.com>
2021-11-02 17:41:53 +0530

    Add files via upload

---
+++ student-contact-management/src/main/java/com/restFulApi/contactManagement/controller/ContactNotFoundException.java
@@ -0,0 +1,15 @@
+package com.restFulApi.contactManagement.controller;
+
+public class ContactNotFoundException extends Exception {
+
+//	/**
+//	 * 
+//	 */
+//	private static final long serialVersionUID = 1L;
+//
+//
+//	public public ContactNotFoundException(String message) {
+//		super(message);
+//	}
+
+}
