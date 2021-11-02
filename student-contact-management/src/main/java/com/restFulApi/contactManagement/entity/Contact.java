54e93ffda53bf3ebb4de3ca30f3d089b7f816414
aarthiigurunathan <36591431+aarthiigurunathan@users.noreply.github.com>
2021-11-02 17:27:58 +0530

    Add files via upload

---
+++ student-contact-management/src/main/java/com/restFulApi/contactManagement/entity/Contact.java
@@ -0,0 +1,52 @@
+package com.restFulApi.contactManagement.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Contact {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String email;
+
+	private Long phoneNumber;
+	
+	public Contact() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(Long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "Contact [id=" + id + ", email=" + email + ", phoneNumber=" + phoneNumber + "]";
+	}
+
+	
+}
