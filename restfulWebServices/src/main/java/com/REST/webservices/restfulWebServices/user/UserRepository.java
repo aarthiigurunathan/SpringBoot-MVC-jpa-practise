17155b4c7fdac050e34da2c5cd69dd19eae79bcb
aarthiigurunathan <36591431+aarthiigurunathan@users.noreply.github.com>
2021-11-02 17:31:28 +0530

    Add files via upload

---
+++ restfulWebServices/src/main/java/com/REST/webservices/restfulWebServices/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.REST.webservices.restfulWebServices.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+}
---
+++ restfulWebServices/target/classes/data.sql
@@ -0,0 +1,3 @@
+insert into user values(1,sysdate(),'Kohli');
+insert into user values(2,sysdate(),'Dhoni');
+insert into user values(3,sysdate(),'Rohit');
